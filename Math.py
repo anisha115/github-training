eab63adc59e39ca304854cbceb5bf1ac6d04fefe
anisha115 <60575028+anisha115@users.noreply.github.com>
2021-05-28 14:39:20 +0530

    initial commit of Math.py

---
+++ Math.py
@@ -0,0 +1,15 @@
+#Add implementation
+def add(x,y):
+    return x+y
+
+#subtract implementation
+def subtract(x,y):
+    pass
+def multiply(x,y):
+    return x*y	#bug456
+def divide(x,y):
+    return x/y #on master
+
+#implementation of square
+def square(x):
+	pass
